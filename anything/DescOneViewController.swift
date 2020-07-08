07f6e42e21bb2916d64672cbd7d885a391ecd5b9
Aleksa <aleksa.khruleva@gmail.com>
2020-07-08 07:29:36 +0300

    first commit

---
+++ anything/DescOneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DescOneViewController.swift
+//  anything
+//
+//  Created by Aleksa on 08.07.2020.
+//  Copyright © 2020 Владислав Вечерковский. All rights reserved.
+//
+
+import UIKit
+
+class DescOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Who am I?"
+
+    }
+    
+}
---
+++ anything/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  anything
+//
+//  Created by Aleksa on 08.07.2020.
+//  Copyright © 2020 Владислав Вечерковский. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Our team"
+
+    }
+
+}
