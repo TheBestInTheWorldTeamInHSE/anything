382aef8eecce6c5e7ff8a911f08bc0874fd8b397
Aleksa <aleksa.khruleva@gmail.com>
2020-07-08 07:59:57 +0300

    second commit

---
+++ anything/DescriptionFirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DescriptionFirstViewController.swift
+//  anything
+//
+//  Created by Aleksa on 08.07.2020.
+//  Copyright © 2020 Владислав Вечерковский. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "TEST"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
