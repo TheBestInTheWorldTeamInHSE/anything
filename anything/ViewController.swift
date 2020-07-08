9a5749c89f4929c28049e6c94536cb4467fd8d32
Aleksa <aleksa.khruleva@gmail.com>
2020-07-08 09:51:07 +0300

    added text about me

--- anything/ViewController.swift
+++ anything/ViewController.swift
@@ -12,13 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-    
-    // Testing comment
-    //hello
-    
-    // Another com
-
 }
 
